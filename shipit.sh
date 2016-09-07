5cd79eecdf02b273d5512f2f28d3fdd0dc194c1c
Eric Hacke <eric.hacke@groupbyinc.com>
2016-09-07 10:11:13 -0400

    Fix shipit

--- shipit.sh
+++ shipit.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 rm -rf cdn
+rm -rf api-javascript
 
 set -e
-git clone https://github.com/groupby/api-javascript.git cdn/
+git clone https://github.com/groupby/cdn.git cdn/
 git clone -b gh-pages https://github.com/groupby/api-javascript.git api-javascript/
 
 name=gb-tracker-client
 currentVersion=`cat package.json | jq -r .version`
 
 ## Create current version
-# NOT YET STABLE
-#cp dist/${name}-${currentVersion}.js ${HOME}/cdn/static/${name}-${currentVersion}.js
-#cp dist/${name}-${currentVersion}.min.js ${HOME}/cdn/static/${name}-${currentVersion}.min.js
+cp dist/${name}-${currentVersion}.js cdn/static/javascript/${name}-${currentVersion}.js
+cp dist/${name}-${currentVersion}.min.js cdn/static/javascript/${name}-${currentVersion}.min.js
+
+cp dist/${name}-${currentVersion}.js api-javascript/dist/${name}-${currentVersion}.js
+cp dist/${name}-${currentVersion}.min.js api-javascript/dist/${name}-${currentVersion}.min.js
 
 ## Create canary
 cp dist/${name}-${currentVersion}.js cdn/static/javascript/${name}-canary.js
@@ -25,6 +28,7 @@ git add static/javascript/${name}-*.js
 git commit -m "Release ${name} v${currentVersion}"
 git push
 
+cd ..
 cd api-javascript/
 git add dist/${name}-*.js
 git commit -m "Release ${name} v${currentVersion}"
