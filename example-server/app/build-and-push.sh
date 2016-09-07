1a1b7eb632cb8948278a3270977c2fdcde3985da
Eric Hacke <eric.hacke@groupbyinc.com>
2016-09-07 00:08:38 -0400

    Update script

--- example-server/app/build-and-push.sh
+++ example-server/app/build-and-push.sh
@@ -11,7 +11,3 @@ docker build -t groupbyinc/${NAME}:${PACKAGE_VERSION}-${COMMIT_VERSION} .
 docker tag -f groupbyinc/${NAME}:${PACKAGE_VERSION}-${COMMIT_VERSION} groupbyinc/${NAME}:latest
 docker push groupbyinc/${NAME}:${PACKAGE_VERSION}-${COMMIT_VERSION}
 docker push groupbyinc/${NAME}:latest
-
-set +e
-echo "Ignore any errors below, just clearing dangling images"
-docker rmi $(docker images -q -f dangling=true) || :
