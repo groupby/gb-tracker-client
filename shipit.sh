cee041aaa4c949600d8c95b9ef7bf9ff160abe22
Eric Hacke <eric.hacke@groupbyinc.com>
2016-10-18 17:20:49 -0400

    Add auto-updates to documentation

--- shipit.sh
+++ shipit.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+set -e
 rm -rf cdn
 rm -rf api-javascript
+rm -rf documentation
 
 set -e
 git clone https://github.com/groupby/cdn.git cdn/
 git clone -b gh-pages https://github.com/groupby/api-javascript.git api-javascript/
+git clone https://github.com/groupby/documentation.git documentation/
 
 name=gb-tracker-client
 currentVersion=$(cat package.json | jq -r .version)
+currentMajor=$(cat package.json | jq -r .version | cut -d '.' -f 1)
 
 ## Create current version
 cp dist/${name}-${currentVersion}.js cdn/static/javascript/${name}-${currentVersion}.js
@@ -16,7 +20,14 @@ cp dist/${name}-${currentVersion}.min.js cdn/static/javascript/${name}-${current
 cp dist/${name}-${currentVersion}.js api-javascript/dist/${name}-${currentVersion}.js
 cp dist/${name}-${currentVersion}.min.js api-javascript/dist/${name}-${currentVersion}.min.js
 
-## Create canary
+## Update latest major version
+cp dist/${name}-${currentVersion}.js cdn/static/javascript/${name}-${currentMajor}.js
+cp dist/${name}-${currentVersion}.min.js cdn/static/javascript/${name}-${currentMajor}.min.js
+
+cp dist/${name}-${currentVersion}.js api-javascript/dist/${name}-${currentMajor}.js
+cp dist/${name}-${currentVersion}.min.js api-javascript/dist/${name}-${currentMajor}.min.js
+
+## Update canary
 cp dist/${name}-${currentVersion}.js cdn/static/javascript/${name}-canary.js
 cp dist/${name}-${currentVersion}.min.js cdn/static/javascript/${name}-canary.min.js
 
@@ -31,5 +42,17 @@ git push
 cd ..
 cd api-javascript/
 git add dist/${name}-*.js
+git commit -m "Release ${name} v${currentVersion}"
+git push
+
+cd ..
+cd documentation/
+sed -i "s/gb-tracker-client-.\{1,3\}\.min\.js/gb-tracker-client-${currentMajor}.min.js/g" src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/00_Installation.md
+sed -i "s/gb-tracker-client-.\{5,10\}\.min\.js/gb-tracker-client-${currentVersion}.min.js/g" src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/00_Installation.md
+
+sed -i "s/gb-tracker-client-.\{1,3\}\.min\.js/gb-tracker-client-${currentMajor}.min.js/g" src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/03_TrackerDetails.md
+sed -i "s/gb-tracker-client-.\{5,10\}\.min\.js/gb-tracker-client-${currentVersion}.min.js/g" src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/03_TrackerDetails.md
+git add src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/00_Installation.md
+git add src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/03_TrackerDetails.md
 git commit -m "Release ${name} v${currentVersion}"
 git push
