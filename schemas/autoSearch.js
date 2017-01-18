53fbbb8559eedb7bfa8f206326aaf614b9f3c7d0
Eric Hacke <eric.hacke@groupbyinc.com>
2017-01-17 20:24:15 -0500

    Latest

--- schemas/autoSearch.js
+++ schemas/autoSearch.js
@@ -113,6 +113,10 @@ module.exports={
               },
               sessionId: {
                 type: 'string'
+              },
+              loginId: {
+                type: 'string',
+                optional: true
               }
             },
             strict: true
@@ -279,6 +283,13 @@ module.exports={
                 ]
               },
               sessionId: {
+                maxLength: 10000,
+                rules: [
+                  'trim',
+                  'lower'
+                ]
+              },
+              loginId: {
                 maxLength: 10000,
                 rules: [
                   'trim',
