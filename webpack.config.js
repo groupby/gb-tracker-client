7c6c41c43e84ebde07edb71a5ea9b95ea07b6089
Eric Hacke <eric.hacke@groupbyinc.com>
2017-01-19 10:01:12 -0500

    Latest

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
   module:  {
     loaders: [
       {
+        test:    /\.js$/,
+        exclude: /node_modules/,
+        loader:  'babel-loader'
+      },
+      {
         test:   /\.json$/,
         loader: "json"
       }
