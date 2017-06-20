d20974bbf27678e6d4c81d46e2c2d8d7400a78d6
Eric Hacke <eric.hacke@groupbyinc.com>
2017-06-20 14:25:50 -0400

    Fix docker deploy script

--- example-server/app/build-and-push.sh
+++ example-server/app/build-and-push.sh
@@ -7,7 +7,6 @@ NAME=recommendations-ref-ui
 
 echo "Building and pushing ${NAME}:${PACKAGE_VERSION}"
 
-docker build -t groupbyinc/${NAME}:${PACKAGE_VERSION} .
-docker tag -f groupbyinc/${NAME}:${PACKAGE_VERSION} groupbyinc/${NAME}:latest
+docker build -t groupbyinc/${NAME}:${PACKAGE_VERSION} -t groupbyinc/${NAME}:latest .
 docker push groupbyinc/${NAME}:${PACKAGE_VERSION}
 docker push groupbyinc/${NAME}:latest
