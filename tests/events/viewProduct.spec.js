3f751573fa2c312ee5339ec2fffdc90af15de4dd
Ben Teichman <ben.l.teichman@gmail.com>
2017-09-11 14:31:45 -0400

    remove number coercing

--- tests/events/viewProduct.spec.js
+++ tests/events/viewProduct.spec.js
@@ -9,7 +9,6 @@ navigator.appCodeName = 'Microsoft Internet Explorer';
 navigator.userAgent = 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)';
 
 const GbTracker = require('../../lib/gb-tracker-full');
-const GbTrackerSlim = require('../../lib/gb-tracker-slim');
 
 describe('viewProduct tests', () => {
   it('should accept valid viewProduct event', (done) => {
@@ -58,56 +57,6 @@ describe('viewProduct tests', () => {
       expect(event.visit.customerData).to.eql(expectedEvent.visit.customerData);
       expect(event.visit.generated.timezoneOffset).to.not.be.undefined;
       expect(event.visit.generated.localTime).to.not.be.undefined;
-      done();
-    };
-
-    gbTrackerCore.setVisitor(expectedEvent.visit.customerData.visitorId, expectedEvent.visit.customerData.sessionId);
-
-    gbTrackerCore.sendViewProductEvent({
-      product: expectedEvent.product
-    });
-  });
-
-  it('should convert price to number when slim', (done) => {
-    const expectedEvent = {
-      product: {
-        productId: 'asdfasd',
-        category: 'boats',
-        collection: 'boatssrus',
-        title: 'boats',
-        sku: 'asdfasf98',
-        price: '100.21'
-      },
-      eventType: 'viewProduct',
-      customer: {
-        id: 'testcustomer',
-        area: 'area'
-      },
-      visit: {
-        customerData: {
-          visitorId: 'visitor',
-          sessionId: 'session'
-        },
-        generated: {
-          uri: '',
-          timezoneOffset: 240,
-          localTime: '2016-08-14T14:05:26.872Z'
-        }
-      }
-    };
-
-    const gbTrackerCore = new GbTrackerSlim(expectedEvent.customer.id, expectedEvent.customer.area);
-
-    gbTrackerCore.setInvalidEventCallback(() => {
-      done('fail');
-    });
-
-    gbTrackerCore.__private.sendEvent = (event) => {
-      if (event.eventType === 'sessionChange') {
-        return;
-      }
-
-      expect(event.product.price).to.be.a('number');
       done();
     };
 
