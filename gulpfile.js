86e1d3347353dab6633f916236f1bdabc15014a4
Eric Hacke <eric.hacke@groupbyinc.com>
2018-01-12 15:41:11 -0500

    update build scripts
    Latest
    
    Latest
    
    Minor
    
    Update schemas
    
    Update schemas

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,19 @@ const mocha    = require('gulp-mocha');
 const eslint   = require('gulp-eslint');
 const istanbul = require('gulp-istanbul');
 const gulpIf   = require('gulp-if');
+const del      = require('del');
+const replace = require('gulp-replace');
 
 const webpack       = require('webpack');
 const webpackStream = require('webpack-stream');
 const packageJson   = require('./package.json');
 const webpackConfig = require('./webpack.config');
+
+gulp.task('clean', () => del([
+  'dist/**/*',
+  'build/**/*',
+  'example-server/app/dist/**/*'
+]));
 
 gulp.task('build:minify', () => {
   const minConfig = Object.assign({}, webpackConfig, {
@@ -23,20 +31,30 @@ gulp.task('build:minify', () => {
   });
 
   return gulp.src(['lib/gb-tracker-window.js'])
-    .pipe(webpackStream(minConfig))
-    .pipe(gulp.dest('dist'));
+  .pipe(webpackStream(minConfig))
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:normal', () => gulp.src(['lib/gb-tracker-window.js'])
-  .pipe(webpackStream(webpackConfig))
-  .pipe(gulp.dest('dist')));
+.pipe(webpackStream(webpackConfig))
+.pipe(gulp.dest('dist')));
+
+gulp.task('build:example-server', () => gulp.src(['lib/gb-tracker-window.js'])
+.pipe(webpackStream(webpackConfig))
+.pipe(gulp.dest('example-server/app/dist')));
+
+gulp.task('update:example-server', ['build:example-server'], () => gulp.src(['example-server/app/public/index.html'])
+.pipe(replace(/dist\/gb-tracker-client-\S+\.js/g, `dist/gb-tracker-client-${packageJson.version}.js`))
+.pipe(gulp.dest('example-server/app/public/')));
 
 gulp.task('build:node', () => gulp.src(['lib/*.js'])
-  .pipe(babel())
-  .pipe(gulp.dest('build')));
+.pipe(babel())
+.pipe(gulp.dest('build')));
 
 gulp.task('build', [
+  'clean',
   'build:normal',
+  'update:example-server',
   'build:node',
   'build:minify'
 ]);
@@ -46,36 +64,31 @@ const isFixed = (file) => {
   return file.eslint != null && file.eslint.fixed;
 };
 
-
 gulp.task('test:dirty', () => {
   return gulp.src('tests/**/*.spec.js')
-    .pipe(mocha({reporter: 'spec'}));
+  .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('pre-test', ['build'], () => {
-  return gulp.src([
-    'lib/gb-tracker-*.js',
-    'lib/utils.js'
-  ])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
-});
+gulp.task('pre-test', ['build'], () => gulp.src([
+  'lib/gb-tracker-*.js',
+  'lib/utils.js'
+])
+.pipe(istanbul())
+.pipe(istanbul.hookRequire()));
 
-gulp.task('test:coverage', ['pre-test'], () => {
-  return gulp.src(['tests/**/*.spec.js'])
-    .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports({
-      reporters: [
-        'text',
-        'html',
-        'lcov'
-      ]
-    }))
-    .once('error', () => {
-      console.error('coverage failed');
-      process.exit(1);
-    });
-});
+gulp.task('test:coverage', ['pre-test'], () => gulp.src(['tests/**/*.spec.js'])
+.pipe(mocha({reporter: 'spec'}))
+.pipe(istanbul.writeReports({
+  reporters: [
+    'text',
+    'html',
+    'lcov'
+  ]
+}))
+.once('error', () => {
+  console.error('coverage failed');
+  process.exit(1);
+}));
 
 const lint = () => {
   return gulp.src([
@@ -83,24 +96,20 @@ const lint = () => {
     'lib/utils.js',
     'tests/**/*.js'
   ], {base: './'})
-    .pipe(eslint({
-      fix: true
-    }))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(gulpIf(isFixed, gulp.dest('./')))
-    .once('error', () => {
-      console.error('lint failed');
-      process.exit(1);
-    });
+  .pipe(eslint({
+    fix: true
+  }))
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError())
+  .pipe(gulpIf(isFixed, gulp.dest('./')))
+  .once('error', () => {
+    console.error('lint failed');
+    process.exit(1);
+  });
 };
 
-gulp.task('test:lint', ['test:coverage'], () => {
-  return lint();
-});
+gulp.task('test:lint', ['test:coverage'], () => lint());
 
-gulp.task('lint', () => {
-  return lint();
-});
+gulp.task('lint', () => lint());
 
 gulp.task('test', ['test:lint']);
