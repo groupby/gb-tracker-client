9003adb4439901b146429faa9374c52590c9a602
Matt Welke <matt.welke@groupbyinc.com>
2019-05-02 16:12:23 -0400

    hotfix webpack configs for cdn filename

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const packageJson = require('../package.json');
 
-const major = packageJson.version.split('.')[0];
+const version = packageJson.version;
 
 module.exports = {
   mode: "development",
   entry:  ['./dist/window.js'],
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: `${packageJson.name}-${major}.js`
+    filename: `${packageJson.name}-${version}.js`
   },
   module: {
     rules: [
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const packageJson = require('../package.json');
+const packageJson = require('../package.json')
 
-const major = packageJson.version.split('.')[0];
+const version = packageJson.version;
 
 module.exports = {
   mode: "production",
   entry:  ['./dist/window.js'],
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: `${packageJson.name}-${major}.min.js`
+    filename: `${packageJson.name}-${version}.min.js`
   },
   module: {
     rules: [
