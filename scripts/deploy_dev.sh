745de6bd0f73425bd30b41b0b6a083c52086de82
Matt Welke <matt.welke@groupbyinc.com>
2020-04-03 16:13:54 -0400

    Clean up shipit script, turn into deploy_dev.sh script.

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -e
 rm -rf cdn
-rm -rf api-javascript
-rm -rf documentation
 
 set -e
 git clone https://github.com/groupby/cdn.git cdn/
-git clone -b gh-pages https://github.com/groupby/api-javascript.git api-javascript/
-git clone https://github.com/groupby/documentation.git documentation/
 
 name=gb-tracker-client
 currentVersion=$(cat package.json | jq -r .version)
@@ -23,11 +19,5 @@ cp build/${name}-${currentVersion}.min.js cdn/static/javascript/${name}-${curren
 
 cd cdn/
 git add static/javascript/${name}-*.js
-git commit -m "Release ${name} v${currentVersion}"
-git push
-
-git add src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/00_Installation.md
-git add src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/100_EventTracking/03_TrackerDetails.md
-git add src/main/webapp/WEB-INF/jsp/wisdom/productv2.x.x/200_adminInternal/02_VerifyingBeaconData.md
 git commit -m "Release ${name} v${currentVersion}"
 git push
