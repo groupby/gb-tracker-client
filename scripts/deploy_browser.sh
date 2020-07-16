d97e0ba48d0bce3795280c003a44b4fcdb741986
Matt Welke <mattwelke@gmail.com>
2020-07-16 12:04:04 -0400

    fixed. i hope.

--- scripts/deploy_browser.sh
+++ scripts/deploy_browser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-NAME="gb-tracker-client-cicd-test"
+NAME="gb-tracker-client"
+NAME_ON_CDN="gb-tracker-client-cicd-test"
 CURRENT_VERSION="$(cat package.json | jq -r .version)"
 CURRENT_VERSION_MAJOR="$(cat package.json | jq -r .version | cut -d '.' -f 1)"
 DEPLOY_DIR="staged_for_deploy"
@@ -13,11 +14,11 @@ npm run buildForBrowser
 mkdir ${DEPLOY_DIR}
 
 # Make major version and specific version files, for both full and minified.
-cp "build/${NAME}-${CURRENT_VERSION}.js" "${DEPLOY_DIR}/${NAME}-${CURRENT_VERSION}.js"
-cp "build/${NAME}-${CURRENT_VERSION}.min.js" "${DEPLOY_DIR}/${NAME}-${CURRENT_VERSION}.min.js"
+cp "build/${NAME}-${CURRENT_VERSION}.js" "${DEPLOY_DIR}/${NAME_ON_CDN}-${CURRENT_VERSION}.js"
+cp "build/${NAME}-${CURRENT_VERSION}.min.js" "${DEPLOY_DIR}/${NAME_ON_CDN}-${CURRENT_VERSION}.min.js"
 
-cp "build/${NAME}-${CURRENT_VERSION}.js" "${DEPLOY_DIR}/${NAME}-${CURRENT_VERSION_MAJOR}.js"
-cp "build/${NAME}-${CURRENT_VERSION}.min.js" "${DEPLOY_DIR}/${NAME}-${CURRENT_VERSION_MAJOR}.min.js"
+cp "build/${NAME}-${CURRENT_VERSION}.js" "${DEPLOY_DIR}/${NAME_ON_CDN}-${CURRENT_VERSION_MAJOR}.js"
+cp "build/${NAME}-${CURRENT_VERSION}.min.js" "${DEPLOY_DIR}/${NAME_ON_CDN}-${CURRENT_VERSION_MAJOR}.min.js"
 
 # Then, copy to bucket.
 gsutil cp -z js "${DEPLOY_DIR}/${NAME}-${CURRENT_VERSION}.js" "gs://${GCS_BUCKET}"
