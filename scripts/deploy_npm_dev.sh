aeb83184ff8c5b64f30ef8c6edb5a0bb99433eb5
Matt Welke <mattwelke@gmail.com>
2020-07-16 13:01:12 -0400

    Testing NPM deploys

--- scripts/deploy_npm_dev.sh
+++ scripts/deploy_npm_dev.sh
@@ -3,6 +3,9 @@
 # Prepare new version of package.json for dev deploy
 CURRENT_VERSION="$(cat package.json | jq -r .version)"
 BETA_VERSION="${CURRENT_VERSION}-beta.0"
-cat package.json | sed "s/${CURRENT_VERSION}/${BETA_VERSION}/g" > package.json
+cat package.json | sed "s/${CURRENT_VERSION}/${BETA_VERSION}/g" > package.json.new
+# Doesn't work without separate cp step.
+cp package.json.new package.json
+rm package.json.new
 
 scripts/deploy_npm.sh
